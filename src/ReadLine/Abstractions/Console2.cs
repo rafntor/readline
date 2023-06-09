31679bfec6cd2a3cbb69495c249581df7e05c66d
Tore Fuglestad <tore@fuglestad.no>
2023-06-09 10:50:59 +0200

    bump test nugets & fix tests

--- src/ReadLine/Abstractions/Console2.cs
+++ src/ReadLine/Abstractions/Console2.cs
@@ -6,38 +6,48 @@ namespace ReadLine
 {
     internal class Console2 : IConsole
     {
-        readonly int _width;
+        readonly int _width = 0;
         public Console2()
         {
             // we want to run with virtual-processing active and that makes it impossible(?) to fully support line-wrapping so here we just try outr best
             // https://github.com/microsoft/terminal/issues/8312#issuecomment-729468976
 
-            _width = Console.BufferWidth;
-
-            if (_width == 0) // embedded with stdout on serial line ? fallback to detection
+            try
             {
-                var left = Console.CursorLeft;
-                Write(Ansi.Cursor.Right(200));
-                _width = Console.CursorLeft + 1;
-                Write(Ansi.Cursor.Left(_width - left));
+                _width = Console.BufferWidth;
+
+                if (_width == 0) // embedded with stdout on serial line ? fallback to detection
+                {
+                    var left = Console.CursorLeft;
+                    Write(Ansi.Cursor.Right(200));
+                    _width = Console.CursorLeft + 1;
+                    Write(Ansi.Cursor.Left(_width - left));
+                }
+            }
+            catch
+            {
             }
         }
         public void CursorAdvance(int count)
         {
-            var left = Console.CursorLeft + count;
-
-            while (left < 0)
+            if (_width > 0)
             {
-                left += _width;
-                Write(Ansi.Cursor.Up(1));
-            }
-            while (left >= _width)
-            {
-                left -= _width;
-                Write(Ansi.Cursor.Down(1));
+                var left = Console.CursorLeft + count;
+
+                while (left < 0)
+                {
+                    left += _width;
+                    Write(Ansi.Cursor.Up(1));
+                }
+                while (left >= _width)
+                {
+                    left -= _width;
+                    Write(Ansi.Cursor.Down(1));
+                }
+
+                count = left - Console.CursorLeft;
             }
 
-            count = left - Console.CursorLeft;
 
             if (count > 0)
                 Write(Ansi.Cursor.Right(count));
